d7e548bdbaf08f87a734e2dfa597905ec1d37d5b
“William <wihezuo26@aoiths.org>
2026-05-01 13:39:44 -0400

    updated data

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,34 @@
-mapboxgl.accessToken = ' ACCESS TOKEN ';
+
+mapboxgl.accessToken = config.MAPBOX_API; 
+
 
 var mapProps = {
-	container: 'map',
-	center: [-73.97, 40.75],
-	zoom: 9
+  container: 'map',   
+  center: [-73.97, 40.75],  
+  zoom: 9             
 };
 
 var map = new mapboxgl.Map(mapProps);
 
+var marker = new mapboxgl.Marker()
+  .setLngLat([-73.97, 40.75]) 
+  .addTo(map);
+
+map.on('click', function (e) {
+  var coordinates = e.lngLat;
+  alert("You clicked the map at: " + coordinates);
+});
+
+marker.getElement().addEventListener('mouseenter', function() {
+  marker.getElement().style.backgroundColor = "red";
+  console.log("Mouse entered the marker!");
+});
+
+marker.getElement().addEventListener('mouseleave', function() {
+  marker.getElement().style.backgroundColor = "";
+  console.log("Mouse left the marker!");
+});
+
+marker.on('dragend', function() {
+  alert('You moved the marker!');
+});
